198efb55f7354525cf5dbc4cd6a32169f38567e7
tafa2004 <tafadzwamungandaire26@gmail.com>
2026-09-07 15:09:24 +0200

    Created empty BankA ccount class and first objects

---
+++ week2/src/main/java/com/oop/Main.java
@@ -0,0 +1,12 @@
+package com.oop;
+
+public class Main {
+    public static void main(String[] args) {
+
+        BankAccount jacobsAccount = new BankAccount();
+        BankAccount userAccount = new BankAccount();
+
+        System.out.println(jacobsAccount== userAccount);
+  
+    }
+}
